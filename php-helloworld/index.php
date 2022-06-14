4f6e2a5437f745938584f6b3770f7a4528c8f1bd
baker-redhat <105225499+baker-redhat@users.noreply.github.com>
2022-06-14 14:26:10 -0500

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,7 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n";
-print "A change is here!\n";
-print "intentional syntax error"\n;
+print "Hello, World! php version is " . PHP_VERSION . "\n\n";
+print "A change is a coming!\n\n";
+print "A change is here!\n\n";
+print "intentional syntax error\n\n";
+print "syntax error resolved.\n\n";
 ?>
