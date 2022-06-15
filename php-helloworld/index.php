c1d74baf44cb1211cbaeb8aa0c9ac96efef6d72c
Shane <shane@lab02.shanebaker.local>
2022-06-15 09:30:49 -0400

    cleaned some stuff up

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,5 +3,4 @@ print "Hello, World! php version is " . PHP_VERSION . "\n\n";
 print "A change is a coming!\n\n";
 print "A change is here!\n\n";
 print "intentional syntax error\n\n";
-print "syntax error resolved.\n\n";
 ?>
